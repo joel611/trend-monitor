5b6ee0f5c0081fdd9545ea0cb29caa1239a6c00f
Joel Chan <joel.chan@hypthon.com>
2026-01-16 23:09:43 +0800

    feat(api): create drizzle db client

--- apps/api-worker/src/lib/db/index.ts
+++ apps/api-worker/src/lib/db/index.ts
@@ -1,3 +1,7 @@
 import { env } from "cloudflare:workers";
+import { createDbClient } from "./client";
 
-export const db = env.DB;
+export const db = createDbClient(env.DB);
+export { createDbClient } from "./client";
+export type { DbClient } from "./client";
+export * from "./schema";
